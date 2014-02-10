d80ddf45a7a9c962c0e7670f39dae8896f322f48
Valery Fremaux <valery.fremaux@gmail.com>
2014-02-10 15:29:47 +0100

    fix dependencies with filter

--- version.php
+++ version.php
@@ -4,4 +4,4 @@ $plugin->version  = 2013100300;   // The (date) version of this plugin
 $plugin->requires = 2011120500;   // Requires this Moodle version
 $plugin->component = 'local_multiroot';
 $plugin->release = '2.5.0 (build 2013100300)';
-
+$plugins->dependencies = array('filter_multiroot' => 2013100200);
