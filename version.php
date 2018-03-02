bbe16ac2a2cd7f9bae68c9d132ab2ff222f4e71a
valery <valery.fremaux@gmail.com>
2018-03-02 17:59:40 +0100

    resync all

--- version.php
+++ version.php
@@ -27,11 +27,11 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version  = 2013100300;   // The (date) version of this plugin.
-$plugin->requires = 2017110800;   // Requires this Moodle version.
+$plugin->requires = 2017050500;   // Requires this Moodle version.
 $plugin->component = 'local_multiroot';
-$plugin->release = '3.4.0 (build 2013100300)';
+$plugin->release = '3.3.0 (build 2013100300)';
 $plugin->maturity = MATURITY_RC;
 
 // Non moodle attributes.
-$plugin->codeincrement = '3.4.0000';
+$plugin->codeincrement = '3.3.0000';
 
