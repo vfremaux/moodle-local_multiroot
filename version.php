a08508fe0ac79cd0264d9e021d6898558186e685
valery <valery.fremaux@gmail.com>
2020-07-11 15:30:56 +0200

    Update travis defines

--- version.php
+++ version.php
@@ -27,11 +27,11 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version  = 2013100300;   // The (date) version of this plugin.
-$plugin->requires = 2018112800;   // Requires this Moodle version.
+$plugin->requires = 2019051100;   // Requires this Moodle version.
 $plugin->component = 'local_multiroot';
-$plugin->release = '3.6.0 (build 2013100300)';
+$plugin->release = '3.7.0 (build 2013100300)';
 $plugin->maturity = MATURITY_RC;
 
 // Non moodle attributes.
-$plugin->codeincrement = '3.6.0000';
+$plugin->codeincrement = '3.7.0000';
 
